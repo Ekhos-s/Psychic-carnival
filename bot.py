2f03ec7e19d760b63db1b3005489dd0561a871ab
Ekhos-s <elocation2@gmail.com>
2018-02-22 13:20:03 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,16 +20,6 @@ async def on_message(message):
         return
     elif "hello there" in message.content:
         await client.send_message(message.channel, "GENERAL KENOBI!!!!")
-    elif "darth" in message.content:
-        await client.send_message(message.channel, "Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.")
-    elif "senate" in message.content:
-        await client.send_message(message.channel, "I AM THE SENATE")
-    elif message.content == "Tell me a joke.":
-        await client.send_message(message.channel, "The rank of master.")
-    elif "?" in message.content:
-        await client.send_message(message.channel, "What about the droid attack on the wookies?")
-    elif "do it" in message.content or "Do it" in message.content:
-        await client.send_message(message.channel, "dewit!")
     elif "men" in message.content:
         i = 0
         mystring = message.content
